1045ec4cfe16d1db40cde95de9005f61f31c5be2
Edmond Ngai <ed_ngai98@hotmail.com>
2020-04-13 20:17:22 +1000

    Final

--- app/src/main/java/com/example/chucknorris/FullQuoteActivity.java
+++ app/src/main/java/com/example/chucknorris/FullQuoteActivity.java
@@ -1,12 +1,18 @@
 package com.example.chucknorris;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -21,8 +27,10 @@ public class FullQuoteActivity extends AppCompatActivity {
     private DocumentReference quoteRef = db.collection("Quotes").document("Favourites");
     private CollectionReference quoteDatabase = db.collection("Quotes");
 
-    private TextView savedQ, fullQ;
+    private TextView savedQ, fullQ, ratingScore;
     private RatingBar ratingBar;
+    private ImageView editImage;
+    private Button tempButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,9 @@ public class FullQuoteActivity extends AppCompatActivity {
         savedQ = findViewById(R.id.savedAs);
         fullQ = findViewById(R.id.fullQuote);
         ratingBar = findViewById(R.id.ratingBarFull);
+        ratingScore = findViewById(R.id.ratingScore);
+        editImage = findViewById(R.id.editIcon);
+        tempButton = findViewById(R.id.deleteQuote);
 
         getSpecificQuote.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
@@ -60,8 +71,63 @@ public class FullQuoteActivity extends AppCompatActivity {
                     savedQ.setText(quotes_name);
                     fullQ.setText(quotes_description);
                     ratingBar.setRating(Float.parseFloat(quotes_rating));
+                    ratingScore.setText(quotes_rating);
 
                 }
+            }
+        });
+
+
+        //Saved Quote button
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(FullQuoteActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.edit_quotes, null);
+                final EditText mNewQuote = (EditText) mView.findViewById(R.id.newQuote);
+                final EditText mNewRating = (EditText) mView.findViewById(R.id.newRating);
+                Button mSave = (Button) mView.findViewById(R.id.btnSave);
+                mSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!mNewQuote.getText().toString().isEmpty() && !mNewRating.getText().toString().isEmpty()) {
+
+                            String saveQAs = mNewQuote.getText().toString();
+                            String saveRAs = mNewRating.getText().toString();
+                            String sameQ = fullQ.getText().toString();
+
+                            SavedQuotes quotes = new SavedQuotes(saveQAs, sameQ, saveRAs);
+
+                            getSpecificQuote.set(quotes);
+                            Toast.makeText(FullQuoteActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(FullQuoteActivity.this, "Please fill in empty fields", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
+
+        //Delete quote button
+        tempButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder2 = new AlertDialog.Builder(FullQuoteActivity.this);
+                View mView2 = getLayoutInflater().inflate(R.layout.delete_alert, null);
+                Button mDelete = (Button) mView2.findViewById(R.id.btnSave);
+                mDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getSpecificQuote.delete();
+                    }
+                });
+                mBuilder2.setView(mView2);
+                AlertDialog dialog2 = mBuilder2.create();
+                dialog2.show();
             }
         });
     }
